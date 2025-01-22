9fd43937f8462cb0702df2fbaea998c296ae59a1
EMarvelM <marvelousegbe19@gmail.com>
2025-01-22 15:03:09 +0100

    C# Hello World!

---
+++ HelloWorldProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloApp {
+    class Program {
+        static void Main( string[] arg ) {
+            string? input = Console.ReadLine();
+            string name = input ?? "Guest";
+
+
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
