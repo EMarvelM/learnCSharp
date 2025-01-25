32f21a46307d032ce5ca60e8fd6d6e0978be03c3
EMarvelM <marvelousegbe19@gmail.com>
2025-01-25 16:33:21 +0100

    using the Data namespace for eval calculation

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,19 +1,73 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Data;
 
 namespace CalculatorApp {
-    class Program {
-        List<string> operations = ["addition", "subtraction", "multiplication", "division"];
+    abstract class Calculate {
+        protected abstract void printHead(string arg);
+
+        private void doCalculate(string sign="") {
+            Console.Write("Enter your input in this format (1 + 2 + 4 . . .) >");
+            string? input = Console.ReadLine();
+            string line = input ?? null;
+            if (string.IsNullOrEmpty(line)){
+                Console.WriteLine("[Err]: An error occured while tryig to take input");
+                return;
+            }
+
+            string[] lines= line.Split(sign);
+            var result = 0;
+            int num;
+            foreach (string l in lines) {
+                if (!int.TryParse(l, out num)) {
+                    Console.WriteLine("Invalid input" + l);
+                    continue;
+                }
+
+                result = num + result;
+            }
+            Console.WriteLine(result);
+        }
+
+
+        protected void doAllCalculate() {
+            Console.Write("Enter your input in this format (1 + 2 + 4 . . .) >");
+            string? input = Console.ReadLine();
+            string line = input ?? null;
+            if (string.IsNullOrEmpty(line)){
+                Console.WriteLine("[Err]: An error occured while tryig to take input");
+                return;
+            }
+
+            string value = new DataTable().Compute(line, null).ToString();
+            
+            Console.WriteLine(value);
+        }
+
+        public void addition() {
+            Console.WriteLine(new string('\n', 10));
+            printHead("ADDITIONS");
+
+            doAllCalculate();
+            // doCalculate("+");
+
+            // implement addition logic
+        }
+    }
+
+    class Program : Calculate {
+        private List<string> operations = ["addition", "subtraction", "multiplication", "division"];
 
         private void printNow(string? arg = "\n") {
             Console.Write(arg);
+            Console.WriteLine("Available Options: ");
             foreach (string op in operations) {
                 Console.Write($"{operations.IndexOf(op)}. ");
                 Console.WriteLine(char.ToUpper(op[0]) + op.Substring(1));
             }
         }
 
-        private void printHead(string arg = "") {
+        protected override void printHead(string arg = "") {
             string equalsTo = new string('=' , 80);
             Console.WriteLine($"{equalsTo}\n{arg}\n{equalsTo}");
         }
@@ -46,11 +100,12 @@ namespace CalculatorApp {
                 }
                 
                 // switch statement to process calculation
-                switch (response) {
-                    case "addtion":
-                        // case for addition
-                        break;
-                }
+                // switch (response) {
+                //     case "addition":
+                //         program.addition();
+                //         break;
+                // }
+                program.doAllCalculate();
             }
         }
     }
