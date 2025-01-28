088359e4cef4c4fe44441f114c6b03e27435ecb7
EMarvelM <marvelousegbe19@gmail.com>
2025-01-28 08:38:11 +0100

    Encapsulation: Learning by Doing

---
+++ OOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP {
+	class Temperature {
+		private decimal _celsius;
+
+		public decimal Celsius {
+			get => _celsius;
+
+			set {
+				if (value < (decimal)-273.15)
+					throw new Exception("Celsius cannot be less than 237.15");
+
+				_celsius = value;
+			}
+		}	
+
+		public decimal Fahrenheit {
+			get => (_celsius * 9/5) + 32;
+		}
+	}
+
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello, World!");
+
+			Temperature temp = new Temperature();
+			temp.Celsius = 9.1M;
+
+			Console.WriteLine(temp.Fahrenheit);
+		}
+	}
+}
+
+
+// See https://aka.ms/new-console-template for more information
